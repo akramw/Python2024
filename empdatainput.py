f9473ac6422bac5fdbb1ca45df5ed295e6b197d6
Md Wasim Akram <59029983+akramw@users.noreply.github.com>
2024-07-18 21:42:13 +0530

    Updated 2 files: read_emp_data.py, read_multiple_value.py

---
+++ empdatainput.py
@@ -0,0 +1,16 @@
+#### this program wile read the Employee data from keybood
+
+
+eid=int(input('Enter your ID:'))
+enem=input('Enter your Name:')
+esal=float(input('Enter your Salary:'))
+eadd=input('Enter your Address:')
+emarr=bool(input('Are you Marred:[True/Fasl]:'))
+
+print('Thais is the Employee Deatiels....')
+
+print('Employe is ID:', eid)
+print('Employe Name:', enem)
+print('Employee Salary:', esal)
+print('Employee Address:', eadd)
+print('Employe is Marred', emarr)
